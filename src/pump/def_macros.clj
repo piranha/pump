cc37e77d72c2177bba737ce58140999426974947
Vsevolod Solovyov <vsevolod.solovyov@gmail.com>
2013-09-28 10:51:56 +0300

    defr macro now converts dashed-names in props to camelCase

--- src/pump/def_macros.clj
+++ src/pump/def_macros.clj
@@ -1,5 +1,20 @@
-(ns pump.def-macros)
+(ns pump.def-macros
+  (:require [clojure.string :as string]))
+
+(defn dash-to-camel-name
+  [name]
+  (let [words (string/split name #"-")
+        camels (map string/capitalize (rest words))
+        complete (apply str (first words) camels)]
+    complete))
+
+(defn dash-to-camel-keys
+  [hashmap]
+  (into {} (for [[k v] hashmap]
+             [(keyword (dash-to-camel-name (name k))) v])))
 
 (defmacro defr
   [name body]
-  `(def ~name (pump.core/react (assoc ~body :displayName name))))
+  (let [cameled# (dash-to-camel-keys body)
+        body# (assoc cameled# :displayName name)]
+    `(def ~name (pump.core/react ~body#))))
